c71942a7ab521575817ed683eb3bf0cb812e471d
yn1046 <22590687+yn1046@users.noreply.github.com>
2023-01-08 22:52:08 +0300

    Fix distance algorithm

--- main.py
+++ main.py
@@ -73,9 +73,24 @@ def distance(token1, token2):
 
     for t1 in range(len(token1) + 1):
         distances[t1][0] = t1
-
     for t2 in range(len(token2) + 1):
         distances[0][t2] = t2
+
+    for t1 in range(1, len(token1) + 1):
+        for t2 in range(1, len(token2) + 1):
+            if token1[t1 - 1] == token2[t2 - 1]:
+                distances[t1][t2] = distances[t1 - 1][t2 - 1]
+            else:
+                a = distances[t1][t2 - 1]
+                b = distances[t1 - 1][t2]
+                c = distances[t1 - 1][t2 - 1]
+
+                if a <= b and a <= c:
+                    distances[t1][t2] = a + 1
+                elif b <= a and b <= c:
+                    distances[t1][t2] = b + 1
+                else:
+                    distances[t1][t2] = c + 1
 
     return distances[len(token1)][len(token2)]
 
@@ -114,11 +129,8 @@ def main():
 
         return_code = 1
 
-        # if args.show_diff:
-        #     print('\n'.join(difflib.ndiff(a[1].splitlines(), b[1].splitlines())))
-
     with open(args.output, "w") as output_file:
-        output_file.write('\n'.join([str(round(fl, 2)) for fl in scores]))
+        output_file.write('\n'.join([str(round(fl / 100, 3)) for fl in scores]))
 
     exit(return_code)
 
